66c5b668ee97b97fe5b2100929cd256c19e10c67
Alex Pim <pimalex1978@yandex.ru>
2025-08-10 22:43:21 +0300

    Lf to CRLF

--- _32_ocr/convert_jpg_to_txt.py
+++ _32_ocr/convert_jpg_to_txt.py
@@ -31,7 +31,7 @@ def image_to_text_easyocr(my_image_path: str, output_file=None, my_scope_languag
     """
     Конвертирует изображение с текстом в текстовый файл с помощью EasyOCR.
 
-    Путь к файлу передавать без всяких кавычек, например: C:\Users\pimal\PycharmProjects\photo_2025-08-10_code.jpg
+    Путь к файлу передавать без всяких кавычек, например: C:\Users\pl\PycharmProjects\photo_2025-08-10_code.jpg
 
     :param my_image_path: путь к изображению (JPG/PNG)
     :param output_file: путь к выходному файлу (если None, создаётся автоматически)
